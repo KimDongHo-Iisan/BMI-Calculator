fe76a70e4c6dde59b68dbdf5b1eeb84da186cfdb
김동호 <68005740+KimDongHo-Iisan@users.noreply.github.com>
2021-07-06 00:29:45 +0900

    BMI 3

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -9,18 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //슬라이더가 움직일때 값이 변화되는 것을 보여주기 위해 아웃렛을 추가.
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
     @IBAction func heightSliderChanged(_ sender: UISlider) {
-        print(String(format: "%.2f", sender.value)) // 소숫점 2자리까지 출력하게 하는 코드.
+        heightLabel.text = (String(format: "%.2f", sender.value))
     }
     
     @IBAction func weightSliderChanged(_ sender: UISlider) {
-        print(Int(sender.value)) // 몸무게는 소숫점까지 필요하지 않기때문에 INT로 변형해주어도 된다.
+        weightLabel.text = print(Int(sender.value)) // 이렇게 코드를 짜면, text는 String을 원하는데, INT 값이 출력되어서 에러가 발생한다.
     }
 }
 
