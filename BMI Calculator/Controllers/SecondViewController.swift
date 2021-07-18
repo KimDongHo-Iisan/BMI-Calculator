947a743fdb29a4a57147aec1e40a018f48598044
김동호 <68005740+KimDongHo-Iisan@users.noreply.github.com>
2021-07-18 17:53:29 +0900

    직접 만들어보는 viewController

---
+++ BMI Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by Dongho kim on 2021/07/18.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
