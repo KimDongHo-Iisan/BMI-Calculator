8cf18f3b46b6080dd7805ab276559089ab30b157
김동호 <68005740+KimDongHo-Iisan@users.noreply.github.com>
2021-07-19 11:52:25 +0900

    4

--- OptionalsDemo/OptionalsDemo/main.swift
+++ OptionalsDemo/OptionalsDemo/main.swift
@@ -1,16 +1,18 @@
-// if문을 통해 nil값을 피하는 방법
+//만약 옵셔널 값이 nil이 아니면 safeOptional 값을 출력해라.
+/* if let safeOptional = optional {
+            safeOptional
+ */
 
+let myOptioanl: String?
 
-let myOptional: String?
+myOptioanl = nil
 
-myOptional = "angle"
-
-if myOptional != nil {
-    let text: String = myOptional!
-    
+if let safeOptional = myOptioanl {
+    let text: String = safeOptional
+    let text2: String = safeOptional
+    print(safeOptional)
 } else {
-    print("myOptional was found to be nil")
+    print("myOptioanl was found to be nil")
 }
 
-//if문을 통해 nil값이 아니라면, 값을 출력하도록 하는것.
-//하지만 이 방법은 상당히 코드가 길어지고, 여러개의 값을 출력하고 싶을때도 무조건 강제적 언래핑을 사용해야한다는 불편함이 있다. 
+// 이 방법은 만약 옵셔널값이 nil이면 출력되는 값이 없다는 것이 단점.
