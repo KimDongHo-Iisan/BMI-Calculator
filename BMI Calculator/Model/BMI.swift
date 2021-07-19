5d5cffd95f2f0ab9298da0a5217ccb751562007d
김동호 <68005740+KimDongHo-Iisan@users.noreply.github.com>
2021-07-20 01:15:50 +0900

    완성

---
+++ BMI Calculator/Model/BMI.swift
@@ -0,0 +1,17 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Dongho kim on 2021/07/20.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+//BMI 구조체
+struct BMI {
+    let value: Float?
+    let advice: String?
+    let color: UIColor? //UIKit으로 변경하지 않으면, UIColor를 사용할수 없다. 주의
+}
+
--- BMI Calculator/Model/CalculatorBrain.swift
+++ BMI Calculator/Model/CalculatorBrain.swift
@@ -6,23 +6,38 @@
 //  Copyright © 2021 Angela Yu. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 struct CalculatorBrain {
     
-    var bmi: Float? //값이 존재하지 않을 가능성이 있기 때문에 옵셔널을 사용해야하는데, 이렇게 되면 bmi값을 출력할때 nil값이 나올 가능성이 있어서 에러가 발생함.
-
-    //bmivalue는 현재 String으로 받고 있기 때문에 리턴값을 String으로 설정.
+    var bmi: BMI? // BMI 구조체를 사용
+    
     func getBMIValue() -> String {
-        let bmiTo1DecimalPlace = String(format: "%.lf", bmi)//bmi값을 소수점 1번째자리까지 출력하도록 함.
+        let bmiTo1DecimalPlace = String(format: "%.lf", bmi?.value ?? 0.0)//bmi값을 소수점 1번째자리까지 출력하도록 함. bmi가 옵셔널이면 기본값으로 0.0을 출력해라.
         return bmiTo1DecimalPlace
     }
     
-    //BMI 계산을 하기 위한 함수.-> 구조체 내에서는 불변성을 없애는 메소드를 만들었을때 mutaing을 이용해서 불변성을 없애줘야 이 메소드를 사용할수 있다.
-    mutating func calculateBMI(height: Float, weight: Float){
-        bmi = weight / (height * height)
+    func getAdvice() -> String {
+        return bmi?.advice ?? "No Advice" //구조체나 클래스의 옵셔널을 해결하기 위한 방법을 이용하였다. bmi?.adivce 이로써 bmi. 구조체의 속성들을 사용할수 있다. 옵셔널이 아닌 경우에 
     }
-
-
+    
+    func getColor() -> UIColor {
+        return bmi?.color ?? UIColor.white
+    }
+    
+    mutating func calculateBMI(height: Float, weight: Float){
+        let bmiValue = weight / (height * height)
+        
+        //bmi 값에 따른 출력들
+        if bmiValue < 18.5 {
+            bmi = BMI(value: bmiValue, advice: "Eat more pies!", color: #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1))
+        } else if bmiValue < 24.9 {
+            bmi = BMI(value: bmiValue, advice: "Fit as a fiddle!", color: #colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1))
+        } else {
+            bmi = BMI(value: bmiValue, advice: "Eat less pies!", color: #colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1))
+        }
+    }
+    
+    
 }
 
