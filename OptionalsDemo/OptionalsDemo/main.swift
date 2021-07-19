c1da7a32f3122f6750f642181c23efb20c29ff18
김동호 <68005740+KimDongHo-Iisan@users.noreply.github.com>
2021-07-19 11:44:34 +0900

    if문을 통해 nil값 피하기

--- OptionalsDemo/OptionalsDemo/main.swift
+++ OptionalsDemo/OptionalsDemo/main.swift
@@ -1,9 +1,16 @@
-//강제적 언래핑
+// if문을 통해 nil값을 피하는 방법
+
 
 let myOptional: String?
-myOptional = "Angla"
 
-let text: String = myOptional!
+myOptional = "angle"
 
-// 컴퓨터에게 이 변수에는 무조건 nil값이 없다고 확신시켜주는 방법. 이 방법에는 단점이 있는 것이, optional이 nil인것을 개발자가 캐치하지 못하면 에러가 발생함.
+if myOptional != nil {
+    let text: String = myOptional!
+    
+} else {
+    print("myOptional was found to be nil")
+}
 
+//if문을 통해 nil값이 아니라면, 값을 출력하도록 하는것.
+//하지만 이 방법은 상당히 코드가 길어지고, 여러개의 값을 출력하고 싶을때도 무조건 강제적 언래핑을 사용해야한다는 불편함이 있다. 
